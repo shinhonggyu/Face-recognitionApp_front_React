b219e843c35e4e31f979063f2784e737d15f7639
shinhonggyu <zowoz0@naver.com>
2021-04-14 01:17:46 +0900

    ImageLinkForm component, Rank component, Particles component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 70,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
