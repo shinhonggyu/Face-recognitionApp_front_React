2f3c27e7c3aa11654b6dcaf86be3ee28a2c92dfe
shinhonggyu <zowoz0@naver.com>
2021-04-22 21:00:00 +0900

    final..

--- src/App.js
+++ src/App.js
@@ -27,23 +27,25 @@ const particlesOptions = {
   },
 };
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: '',
+  },
+};
+
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: '',
-      },
-    };
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -101,7 +103,8 @@ class App extends React.Component {
                   entries: count,
                 },
               });
-            });
+            })
+            .catch(console.log);
         }
         this.displayFaceBox(this.calculaterFaceLocation(response));
       })
@@ -110,7 +113,7 @@ class App extends React.Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     } else if (route === 'home') {
       this.setState({ isSignedIn: true });
     }
